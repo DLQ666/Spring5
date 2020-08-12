fdf64156b22627e372a3bd6425360baaaff9cc49
D·Teach <1114626450@qq.com>
2020-08-12 23:40:35 +0800

    Spring5实例以及新特性

--- spring5_demo6/src/com/dlq/spring5/config/TxConfig.java
+++ spring5_demo6/src/com/dlq/spring5/config/TxConfig.java
@@ -5,6 +5,7 @@ import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.ComponentScan;
 import org.springframework.context.annotation.Configuration;
 import org.springframework.jdbc.core.JdbcTemplate;
+import org.springframework.jdbc.datasource.DataSourceTransactionManager;
 import org.springframework.transaction.annotation.EnableTransactionManagement;
 
 import javax.sql.DataSource;
